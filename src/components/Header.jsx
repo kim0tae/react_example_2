f991191dbd501dd2284bb6614d4cdfd9d26cda3f
kim0tae <kimyt0402@gmail.com>
2025-08-23 17:02:46 +0900

    [FEATURE] tailwincss 설치 및 설정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
-import logo from "../assets/logo.png";
-import "./Header.css";
+import logo from '../assets/logo.png';
+import './Header.css';
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+    <header className='flex flex-col items-center mt-8 mb-16'>
+      <img
+        src={logo}
+        alt='A canvas'
+        className='mb-8 w-44 h-44 object-contain'
+      />
+      <h1
+        className='text-4xl font-semibold tracking-widest 
+      text-center uppercase text-amber-800 font-light'
+      >
+        ReactArt
+      </h1>
+      <p style={{ color: 'red', textAlign: 'center' }}>
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
