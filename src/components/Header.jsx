7be6e3d0f7c084dd24c42d0f8cdcdd272e1767d0
kim0tae <kimyt0402@gmail.com>
2025-08-23 17:19:02 +0900

    [UPDATE] tailwind css 추가 및 사용법

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,62 +1,9 @@
-import { styled } from 'styled-components';
 
 import logo from '../assets/logo.png';
-import classes from './Header.module.css';
+import './Header.module.css';
 
-const StyledHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-
-  & img {
-    object-fit: contain;
-    margin-bottom: 2rem;
-    width: 11rem;
-    height: 11rem;
-  }
-
-  & h1 {
-    font-size: 1.5rem;
-    font-weight: 600;
-    letter-spacing: 0.4em;
-    text-align: center;
-    text-transform: uppercase;
-    color: #9a3412;
-    font-family: 'Pacifico', cursive;
-    margin: 0;
-  }
-
-  & p {
-    text-align: center;
-    color: #a39191;
-    margin: 0;
-  }
-
-  @media (min-width: 768px) {
-    & {
-      margin-bottom: 4rem;
-    }
-
-    & h1 {
-      font-size: 2.25rem;
-    }
-  }
-`;
-
-import logo from '../assets/logo.png';
-import './Header.css';
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt='A canvas' />
-      <h1>ReactArt</h1>
-      <p className={`classes.paragraph`}>
-        A community of artists and art-lovers.
-      </p>
-    </StyledHeader>
     <header className='flex flex-col items-center mt-8 mb-16'>
       <img
         src={logo}
@@ -65,11 +12,11 @@ export default function Header() {
       />
       <h1
         className='text-4xl font-semibold tracking-widest 
-      text-center uppercase text-amber-800 font-light'
+      text-center uppercase text-amber-800'
       >
         ReactArt
       </h1>
-      <p style={{ color: 'red', textAlign: 'center' }}>
+      <p className='text-stone-500'>
         A community of artists and art-lovers.
       </p>
     </header>
